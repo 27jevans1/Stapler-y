c119637c1c5b3e9278a0d9775ea7c3526b4add4e
Josh Evans <27jevans1@go.dsdmail.net>
2026-05-05 13:06:07 -0600

    build.bat fix attempt 2

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
-pyinstaller --clean --noconfirm --onefile --windowed --name "Stapler-y" --paths src src/main.py
+pyinstaller --clean --noconfirm --onefile --windowed --name "Stapler-y" ^
+    --paths src ^
+    --hidden-import chat_win ^
+    --hidden-import ai ^
+    --hidden-import history ^
+    --hidden-import elderCore ^
+    src/main.py
